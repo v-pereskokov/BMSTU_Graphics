58ab9a8797a167d711e9faa7bbb98bfacb4321be
vladpereskokov <vladpereskokov@mail.ru>
2017-09-01 12:58:11 +0300

    add const

--- src/index.js
+++ src/index.js
@@ -4,10 +4,14 @@ const ctx = canvas.getContext('2d');
 const size = 10;
 
 ctx.fillStyle = '#00aa00';
-ctx.fillRect(10, 10, 10, 10);
+ctx.fillRect(size, size, size, size);
 
 ctx.fillStyle = '#000000';
 
 for (let i = 0; i < size; ++i) {
-  ctx.fillRect(i + 10, i + 10, 1, 1);
+  ctx.fillRect(i + size, i + size, 1, 1);
 }
+
+canvas.addEventListener('click', () => {
+
+});
