7ce745844be183b8c92f347391e69d9cde952343
vladpereskokov <vladpereskokov@mail.ru>
2017-09-01 13:07:59 +0300

    add point

--- src/index.js
+++ src/index.js
@@ -7,6 +7,8 @@ ctx.fillStyle = '#00aa00';
 ctx.fillRect(size, size, size, size);
 
 canvas.addEventListener('click', event => {
+  ctx.fillRect(event.offsetX, event.offsetY, 1, 1);
+
   ctx.fillStyle = '#000000';
 
   for (let i = 0; i < size; ++i) {
