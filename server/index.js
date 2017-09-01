7e389338b52c0d1fdf68d6be54d6c608f233f085
vladpereskokov <vladpereskokov@mail.ru>
2017-09-01 23:50:51 +0300

    change server

--- server/index.js
+++ server/index.js
@@ -1,10 +1,11 @@
 const express = require('express');
-const parser = require('body-parser');
 const app = express();
 
-app.use('/', express.static('./src'));
+app.use(express.static('src'));
 
-app.use(parser.json());
+app.use('*', (req, res) => {
+  res.sendStatus(404);
+});
 
 app.listen(process.env.PORT || 3000, () => {
   console.log(`App started on port ${process.env.PORT || 3000}`);
