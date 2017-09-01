425e51ac8933ad02f4146d774134dcfce68110c6
vladpereskokov <vladpereskokov@mail.ru>
2017-09-01 12:39:15 +0300

    add diag

--- src/index.js
+++ src/index.js
@@ -1,4 +1,16 @@
 const canvas = document.querySelector('.picture');
 const ctx = canvas.getContext('2d');
 
+const shapes = [];
+for (let i = 0; i < 10; ++i) {
+  shapes[i] = 1
+}
+
+ctx.fillStyle = '#00aa00';
 ctx.fillRect(10, 10, 10, 10);
+
+ctx.fillStyle = '#000000';
+
+for (let i = 0; i < 10; ++i) {
+  ctx.fillRect(i + 10, i + 10, 1, 1);
+}
