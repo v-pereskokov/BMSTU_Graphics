d52b74eac0a3c35b7c6e4670a986f2cc7dac39db
vladpereskokov <vladpereskokov@mail.ru>
2017-09-01 12:23:14 +0300

    super fun

--- src/index.js
+++ src/index.js
@@ -1,2 +1,5 @@
-const canvas = document.querySelector('picture');
+const canvas = document.querySelector('.picture');
 const ctx = canvas.getContext('2d');
+
+ctx.fillRect(10, 10, 10, 10);
+ctx.fill();
