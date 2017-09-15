11c3e602d282172e38c5cc0fe7901af183926d92
vladpereskokov <vladpereskokov@mail.ru>
2017-09-15 14:54:59 +0300

    change canvas

--- src/canvas/canvas.js
+++ src/canvas/canvas.js
@@ -1,8 +1,14 @@
 export default class Canvas {
-  constructor(className = 'picture', context = '2d') {
+  constructor(className = 'picture', context = '2d', {width, height}) {
     this._canvas = document.querySelector(`.${className}`);
     this._context = this.canvas.getContext(context);
+
+    this._width = width;
+    this._height = height;
+
+    this._setupCanvas();
   }
+
 
   get context() {
     return this._context;
@@ -14,5 +20,10 @@ export default class Canvas {
 
   setCallback(type = 'click', callback) {
     this.canvas.addEventListener(type, callback);
+  }
+
+  _setupCanvas() {
+    this.canvas.width = this._width;
+    this.canvas.height = this._height;
   }
 }
