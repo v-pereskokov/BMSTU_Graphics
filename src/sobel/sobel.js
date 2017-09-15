cf82b1ea5c747797ab9210fb34317f8266823fcf
vladpereskokov <vladpereskokov@mail.ru>
2017-09-15 16:40:45 +0300

    change sobel

--- src/sobel/sobel.js
+++ src/sobel/sobel.js
@@ -13,16 +13,16 @@ export default class Sobel {
   }
 
   top(pixels) {
-    var pixelsPB = this.escalaCinza(pixels);
-    var horizontal = this.convolucao(pixelsPB, constants.KERNEL_Y);
-    var vertical = this.convolucao(pixelsPB, constants.KERNEL_X);
-    var img = this.criarImageData(pixels.width, pixels.height);
+    let pixelsPB = this.escalaCinza(pixels);
+    let horizontal = this.convolucao(pixelsPB, constants.KERNEL_Y);
+    let vertical = this.convolucao(pixelsPB, constants.KERNEL_X);
+    let img = Sobel.createImageData(pixels.width, pixels.height);
 
-    for (var i = 0; i < img.data.length; i += 4) {
-      var v = Math.abs(vertical.data[i]);
-      var h = Math.abs(horizontal.data[i]);
+    for (let i = 0; i < img.data.length; i += 4) {
+      let v = Math.abs(vertical.data[i]);
+      let h = Math.abs(horizontal.data[i]);
 
-      var s = Math.sqrt(Math.pow(h, 2) + Math.pow(v, 2));
+      let s = Math.sqrt(Math.pow(h, 2) + Math.pow(v, 2));
       img.data[i] = s;
       img.data[i + 1] = s;
       img.data[i + 2] = s;
@@ -33,13 +33,13 @@ export default class Sobel {
   }
 
   escalaCinza(pixels) {
-    var d = pixels.data;
+    let d = pixels.data;
 
-    for (var i = 0; i < d.length; i += 4) {
-      var r = d[i];
-      var g = d[i + 1];
-      var b = d[i + 2];
-      var v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
+    for (let i = 0; i < d.length; i += 4) {
+      let r = d[i];
+      let g = d[i + 1];
+      let b = d[i + 2];
+      let v = 0.2126 * r + 0.7152 * g + 0.0722 * b;
       d[i] = d[i + 1] = d[i + 2] = v;
     }
 
@@ -50,31 +50,31 @@ export default class Sobel {
     if (!window.Float32Array)
       Float32Array = Array;
 
-    var side = Math.round(Math.sqrt(weights.length));
-    var halfSide = Math.floor(side / 2);
+    let side = Math.round(Math.sqrt(weights.length));
+    let halfSide = Math.floor(side / 2);
 
-    var src = pixels.data;
-    var sw = pixels.width;
-    var sh = pixels.height;
+    let src = pixels.data;
+    let sw = pixels.width;
+    let sh = pixels.height;
 
-    var w = sw;
-    var h = sh;
-    var output = {width: w, height: h, data: new Float32Array(w * h * 4)};
-    var dst = output.data;
+    let w = sw;
+    let h = sh;
+    let output = {width: w, height: h, data: new Float32Array(w * h * 4)};
+    let dst = output.data;
 
-    for (var y = 0; y < h; y++) {
-      for (var x = 0; x < w; x++) {
-        var sy = y;
-        var sx = x;
-        var dstOff = (y * w + x) * 4;
-        var r = 0, g = 0, b = 0, a = 0;
+    for (let y = 0; y < h; y++) {
+      for (let x = 0; x < w; x++) {
+        let sy = y;
+        let sx = x;
+        let dstOff = (y * w + x) * 4;
+        let r = 0, g = 0, b = 0, a = 0;
 
-        for (var cy = 0; cy < side; cy++) {
-          for (var cx = 0; cx < side; cx++) {
-            var scy = Math.min(sh - 1, Math.max(0, sy + cy - halfSide));
-            var scx = Math.min(sw - 1, Math.max(0, sx + cx - halfSide));
-            var srcOff = (scy * sw + scx) * 4;
-            var wt = weights[cy * side + cx];
+        for (let cy = 0; cy < side; cy++) {
+          for (let cx = 0; cx < side; cx++) {
+            let scy = Math.min(sh - 1, Math.max(0, sy + cy - halfSide));
+            let scx = Math.min(sw - 1, Math.max(0, sx + cx - halfSide));
+            let srcOff = (scy * sw + scx) * 4;
+            let wt = weights[cy * side + cx];
             r += src[srcOff] * wt;
             g += src[srcOff + 1] * wt;
             b += src[srcOff + 2] * wt;
@@ -92,9 +92,9 @@ export default class Sobel {
     return output;
   }
 
-  criarImageData(w, h) {
-    var canvas = document.createElement('canvas');
-    var context = canvas.getContext('2d');
+  static createImageData(w, h) {
+    const canvas = document.createElement('canvas');
+    const context = canvas.getContext('2d');
 
     return context.createImageData(w, h);
   }
