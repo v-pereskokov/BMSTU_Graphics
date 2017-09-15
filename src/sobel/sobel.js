4fc0d3d03999ed10ae0ee53f23729e73d2f0c2b8
vladpereskokov <vladpereskokov@mail.ru>
2017-09-15 15:13:07 +0300

    change sobel

--- src/sobel/sobel.js
+++ src/sobel/sobel.js
@@ -1,11 +1,79 @@
+import * as constants from '../constant/constant';
+
 export default class Sobel {
   constructor(image, width, height) {
     this._image = image;
+    this._data = this._image.data;
+
     this._width = width;
     this._height = height;
+
+    this._grayscaleData = [];
+    this._sobelData = [];
   }
 
-  getPixelAt() {
+  _transform() {
+    let pixelAt = this._getPixelAtBind(this._data);
 
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const r = pixelAt(x, y, 0);
+        const g = pixelAt(x, y, 1);
+        const b = pixelAt(x, y, 2);
+
+        const avg = (r + g + b) / 3;
+        this._grayscaleData.push(avg, avg, avg, 255);
+      }
+    }
+
+    pixelAt = this._getPixelAtBind(this._grayscaleData);
+
+    for (let y = 0; y < height; y++) {
+      for (let x = 0; x < width; x++) {
+        const pixelX = (
+          (constants.KERNEL_X[0][0] * pixelAt(x - 1, y - 1)) +
+          (constants.KERNEL_X[0][1] * pixelAt(x, y - 1)) +
+          (constants.KERNEL_X[0][2] * pixelAt(x + 1, y - 1)) +
+          (constants.KERNEL_X[1][0] * pixelAt(x - 1, y)) +
+          (constants.KERNEL_X[1][1] * pixelAt(x, y)) +
+          (constants.KERNEL_X[1][2] * pixelAt(x + 1, y)) +
+          (constants.KERNEL_X[2][0] * pixelAt(x - 1, y + 1)) +
+          (constants.KERNEL_X[2][1] * pixelAt(x, y + 1)) +
+          (constants.KERNEL_X[2][2] * pixelAt(x + 1, y + 1))
+        );
+
+        const pixelY = (
+          (constants.KERNEL_Y[0][0] * pixelAt(x - 1, y - 1)) +
+          (constants.KERNEL_Y[0][1] * pixelAt(x, y - 1)) +
+          (constants.KERNEL_Y[0][2] * pixelAt(x + 1, y - 1)) +
+          (constants.KERNEL_Y[1][0] * pixelAt(x - 1, y)) +
+          (constants.KERNEL_Y[1][1] * pixelAt(x, y)) +
+          (constants.KERNEL_Y[1][2] * pixelAt(x + 1, y)) +
+          (constants.KERNEL_Y[2][0] * pixelAt(x - 1, y + 1)) +
+          (constants.KERNEL_Y[2][1] * pixelAt(x, y + 1)) +
+          (constants.KERNEL_Y[2][2] * pixelAt(x + 1, y + 1))
+        );
+
+        const magnitude = Math.sqrt((pixelX * pixelX) + (pixelY * pixelY)) >>> 0;
+
+        this._sobelData.push(magnitude, magnitude, magnitude, 255);
+      }
+    }
+
+    let clampedArray = this._sobelData;
+
+    if (typeof Uint8ClampedArray === 'function') {
+      clampedArray = new Uint8ClampedArray(this._sobelData);
+    }
+
+    clampedArray.toImageData = function () {
+      return Sobel.toImageData(clampedArray, width, height);
+    };
+
+    return clampedArray;
+  }
+
+  _getPixelAtBind(data) {
+    return (x, y, i) => data[((width * y) + x) * 4 + i || 0];
   }
 }
