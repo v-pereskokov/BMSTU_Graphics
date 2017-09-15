818274eac56d24efd520b8d18cd149d7793df61a
vladpereskokov <vladpereskokov@mail.ru>
2017-09-15 14:13:51 +0300

    add image

--- src/constant/constant.js
+++ src/constant/constant.js
@@ -2,10 +2,12 @@ const SQUARE_SIZE = 100;
 const PIXEL_SIZE = 1;
 const SQUARE_COLOR = '#00aa00';
 const POINT_COLOR = '#000000';
+const IMAGE_URL = 'http://itc.ua/wp-content/uploads/2017/09/iphonex-front-crop-top-corner-splash.jpg';
 
 export {
   SQUARE_SIZE,
   PIXEL_SIZE,
   SQUARE_COLOR,
-  POINT_COLOR
+  POINT_COLOR,
+  IMAGE_URL
 };
--- src/index.js
+++ src/index.js
@@ -1,4 +1,12 @@
 import Canvas from './canvas/canvas';
+import * as constants from './constant/constant';
 
 const canvas = new Canvas();
 const ctx = canvas.context;
+
+const image = new Image();
+image.src = constants.IMAGE_URL;
+
+image.addEventListener('load', () => {
+  ctx.drawImage(image, 0, 0);
+});
