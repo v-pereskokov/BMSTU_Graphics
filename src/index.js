681f4c2b600c37aa096db3a9ddcf55b22053a767
vladpereskokov <vladpereskokov@mail.ru>
2017-09-15 14:09:53 +0300

    del helpers

--- src/index.js
+++ src/index.js
@@ -1,13 +1,4 @@
 import Canvas from './canvas/canvas';
-import loadFileAsText from './helpers/loadAndPaint';
-import draw from './helpers/draw';
-import imageToFile from './helpers/imageToFile';
 
 const canvas = new Canvas();
 const ctx = canvas.context;
-
-draw(canvas.canvas, ctx);
-
-document.querySelector('.saveToFile').addEventListener('click', imageToFile.bind(this, canvas.canvas, ctx));
-document.querySelector('.loadText')
-  .addEventListener('click', loadFileAsText.bind(this, canvas.canvas, ctx));
