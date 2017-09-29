13b478a8427530049486c8082f5c8c05bb4e15c5
vladpereskokov <vladpereskokov@mail.ru>
2017-09-29 15:27:49 +0300

    add draq triangle by points

--- src/index.js
+++ src/index.js
@@ -1,46 +1,102 @@
 import Canvas from './canvas/canvas';
-import Sobel from './sobel/sobel';
 import * as constansts from './constant/constant';
 
-const toCanvases = (image) => {
-  const plainCanvas = new Canvas('picture', '2d', {
-    width: image.width,
-    height: image.height
+const canvas = new Canvas('picture', '2d', {
+  width: constansts.WIDTH,
+  height: constansts.HEIGHT
+});
+const ctx = canvas.context;
+
+ctx.fillStyle = constansts.SQUARE_COLOR;
+
+let current = 0;
+let points = [];
+
+ctx.lineWidth = 1;
+ctx.lineJoin = 'round';
+ctx.lineCap = 'round';
+ctx.strokeStyle = '#cc181c';
+
+function drawBrez(context, Xd, Yd, Xf, Yf) {
+  let Dx, Dy, Dx2, Dy2, Dxy, S;
+  let Xinc, Yinc, X, Y;
+
+  if (Xd < Xf) Xinc = 1; else Xinc = -1;
+  if (Yd < Yf) Yinc = 1; else Yinc = -1;
+
+  Dx = Math.abs(Xd - Xf);
+  Dy = Math.abs(Yd - Yf);
+
+  Dx2 = Dx + Dx;
+  Dy2 = Dy + Dy;
+
+  X = Xd;
+  Y = Yd;
+
+  context.moveTo(X, Y);
+  if (Dx > Dy) {
+    S = Dy2 - Dx;
+    Dxy = Dy2 - Dx2;
+    for (let i = 0; i < Dx; i++) {
+      if (S >= 0) {
+        Y = Y + Yinc;
+        S = S + Dxy;
+      } else {
+        S = S + Dy2;
+      }
+
+      X = X + Xinc;
+      context.lineTo(X, Y);
+    }
+  } else {
+    S = Dx2 - Dy;
+    Dxy = Dx2 - Dy2;
+
+    for (let i = 0; i < Dy; i++) {
+      if (S >= 0) {
+        X = X + Xinc;
+        S = S + Dxy;
+      } else {
+        S = S + Dx2;
+      }
+      Y = Y + Yinc;
+      context.lineTo(X, Y);
+    }
+  }
+
+  context.stroke();
+}
+
+canvas.canvas.addEventListener('mousedown', event => {
+  points.push({
+    x: event.pageX - canvas.canvas.offsetLeft,
+    y: event.pageY - canvas.canvas.offsetTop
   });
-  plainCanvas.context.drawImage(image, 0, 0);
 
-  const sobel = new Sobel(plainCanvas.getPixel());
+  console.log(points);
 
-  const sobelCanvas = new Canvas('sobel', '2d', {
-    width: image.width,
-    height: image.height
-  });
-  sobelCanvas.context.putImageData(sobel.image, 0, 0);
-};
-
-document.querySelector('.file').addEventListener('change', event => {
-  const reader = new FileReader();
-
-  reader.addEventListener('load', event => {
-    const image = new Image();
-
-    image.addEventListener('load', () => {
-      toCanvases(image);
-    });
-
-    image.addEventListener('error', () => {
-      console.log('Can\'t open');
-    });
-
-    image.src = event.target.result;
-  });
-
-  reader.readAsDataURL(event.target.files[0]);
+  ++current;
 });
 
-const startImage = new Image();
-startImage.src = constansts.IMAGE_URL;
+canvas.canvas.addEventListener('mouseup', () => {
+  if (current === 4) {
+    ctx.clearRect(0, 0, constansts.WIDTH, constansts.HEIGHT);
+    points = [];
+    current = 0;
+  }
 
-startImage.addEventListener('load', () => {
-  toCanvases(startImage);
+  if (current !== 0 && current !== 1) {
+    ctx.beginPath();
+
+    drawBrez(ctx, points[current - 2].x, points[current - 2].y, points[current - 1].x, points[current - 1].y);
+  }
+
+  if (current === 3) {
+    ctx.beginPath();
+
+    ctx.moveTo(points[current - 1].x, points[current - 1].y);
+    ctx.lineTo(points[0].x, points[0].y);
+
+    ctx.stroke();
+  }
 });
