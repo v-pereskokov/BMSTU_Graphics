5522fff43e2cbdc27b70962177ae4a001a741442
vladpereskokov <vladpereskokov@mail.ru>
2017-10-20 15:47:22 +0300

    add clip func

--- src/index.js
+++ src/index.js
@@ -10,17 +10,100 @@ const stack = [];
 
 const ax = 200;
 const ay = 200;
-const bx = 500;
-const by = 200;
+const bx = 600;
+const by = 300;
 const cx = 500;
 const cy = 500;
 const dx = 200;
 const dy = 500;
+const ex = 100;
+const ey = 300;
 
 const xmin = ax;
 const xmax = cx;
 const ymin = ay;
 const ymax = cy;
+
+
+function cyrcusBeck(stack, line = {
+  first: {
+    x: 0,
+    y: 0
+  },
+  second: {
+    x: 0,
+    y: 0
+  }
+}) {
+  let delX = line.first.x - line.second.x;
+  let delY = line.first.y - line.second.y;
+
+  let pointDel = {
+    x: delX,
+    y: delY
+  };
+
+  let boundaryPoint = stack[2];
+
+  let tEnter = 0;
+  let tLeave = 1;
+
+  for (let i = 0; i < stack.length; ++i) {
+    if (i === stack.length - 2) {
+      break;
+    }
+
+    let point = stack[i];
+    let point2 = stack[i + 1];
+
+    let line2 = {
+      first: point,
+      second: point2
+    };
+
+    let N = getInsideNormal(line2, boundaryPoint);
+
+    let w = {
+      x: line.first.x - point.x,
+      y: line.first.y - point.y
+    };
+
+    let num = scalar(w, N);
+    let den = scalar(pointDel, N);
+
+    if (den === 0) {
+      if (num < 0) {
+        return;
+      }
+
+      continue;
+    }
+
+    let t = -num / den;
+    if (den > 0) {
+      tEnter = Math.max(tEnter, t);
+    } else {
+      tLeave = Math.min(tLeave, t);
+    }
+
+    boundaryPoint = point;
+  }
+
+  if (tEnter > tLeave) {
+    return;
+  }
+
+  return {
+    first: {
+      x: line.first.x + delX * tEnter,
+      y: line.first.y + delY * tEnter
+    },
+    second: {
+      x: line.first.x + delX * tLeave,
+      y: line.first.y + delY * tLeave
+    }
+  }
+}
 
 const drawLine = (x0, y0, x1, y1) => {
   ctx.beginPath();
@@ -44,39 +127,6 @@ const drawTestLine = (x0, y0, x1, y1, color) => {
   ctx.stroke();
 };
 
-function setOutcode(point) {
-  let outcode = '';
-
-  const x = point[0];
-  const y = point[1];
-
-  if (y > ymax) {
-    outcode = outcode + '1';
-  } else {
-    outcode = outcode + '0';
-  }
-
-  if (y < ymin) {
-    outcode = outcode + '1';
-  } else {
-    outcode = outcode + '0';
-  }
-
-  if (x > xmax) {
-    outcode = outcode + '1';
-  } else {
-    outcode = outcode + '0';
-  }
-
-  if (x < xmin) {
-    outcode = outcode + '1';
-  } else {
-    outcode = outcode + '0';
-  }
-
-  return outcode;
-}
-
 function deleteLine(start, end) {
   ctx.beginPath();
   ctx.moveTo(start[0], start[1]);
@@ -88,84 +138,11 @@ function deleteLine(start, end) {
   ctx.stroke();
 }
 
-function findIntersection(outcode, end_points) {
-  let start = end_points[0];
-  let end = end_points[1];
-
-  let x1 = start[0];
-  let x2 = end[0];
-  let y1 = start[1];
-  let y2 = end[1];
-
-  let intersections_list = [];
-  let intersect = [0, 0];
-
-
-  let m = (y2 - y1) / (x2 - x1);
-
-  let c = y1 - m * x1;
-
-  if (outcode.charAt(0) === '1') {
-    intersect[0] = (ymax - c) / m;
-    intersect[1] = ymax;
-
-    intersections_list.push(intersect);
-  }
-
-  if (outcode.charAt(1) === '1') {
-    intersect[0] = (ymin - c) / m;
-    intersect[1] = ymin;
-
-    intersections_list.push(intersect);
-  }
-
-  if (outcode.charAt(2) === '1') {
-    intersect[0] = xmax;
-    intersect[1] = (m * xmax + c);
-
-    intersections_list.push(intersect);
-  }
-
-  if (outcode.charAt(3) === '1') {
-    intersect[0] = xmin;
-    intersect[1] = (m * xmin + c);
-
-    intersections_list.push(intersect);
-  }
-
-  return intersections_list;
-}
-
-function clip(endPoints) {
-  let start = endPoints[0];
-  let end = endPoints[1];
-
-  let o1 = setOutcode(start);
-  let o2 = setOutcode(end);
-
-  if (o1 === '0000' && o2 === '0000') {
-  } else if ((o1 & o2) !== 0) {
-    deleteLine(start, end);
-  } else if ((o1 & o2) === 0 && o1 === '0000' || o2 === '0000') {
-    let intersections = findIntersection(o1, endPoints);
-    if (o1 !== '0000') {
-      deleteLine(start, intersections[0]);
-    } else if (o2 !== '0000') {
-      deleteLine(end, intersections[0]);
-    }
-  } else if ((o1 & o2) === 0) {
-    let intersections = findIntersection(o1, endPoints);
-    deleteLine(start, intersections[0]);
-
-    intersections = findIntersection(o2, endPoints);
-    deleteLine(end, intersections[0]);
-  }
-}
-
 drawLine(ax, ay, bx, by);
 drawLine(bx, by, cx, cy);
 drawLine(cx, cy, dx, dy);
-drawLine(dx, dy, ax, ay);
+drawLine(dx, dy, ex, ey);
+drawLine(ex, ey, ax, ay);
 
 drawTestLine(300, 300, 400, 400, 'ff0211');
 drawTestLine(220, 100, 300, 150, 'ff0211');
